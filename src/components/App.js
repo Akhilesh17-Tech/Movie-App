2d03dbf2cfa7473fd87578cdb3d24259ba4d030f
Akhilesh kushwah <Akhileshkushwah17@gmail.com>
2022-05-03 18:44:14 +0530

    styling done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import { data } from "../data";
+import MovieCard from "./MovieCard";
+import Navbar from "./Navbar";
+import "../components/scss/Style.scss";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Project setup</header>
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
